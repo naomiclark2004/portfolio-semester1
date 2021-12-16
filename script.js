01b1a24841a0a83e0460537a32ec975fddeb6821
Naomi Clark <naomiclark2004@gmail.com>
2021-12-15 23:16:28 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function newWorks(){
+    document.getElementById("box1").style.order="9";
+    document.getElementById("box2").style.order="8";
+    document.getElementById("box3").style.order="7";
+    document.getElementById("box4").style.order="6";
+    document.getElementById("box5").style.order="5";
+    document.getElementById("box6").style.order="4";
+    document.getElementById("box7").style.order="3";
+    document.getElementById("box8").style.order="2";
+    document.getElementById("box9").style.order="1";
+}
+
+function oldWorks(){
+    document.getElementById("box1").style.order="1";
+    document.getElementById("box2").style.order="2";
+    document.getElementById("box3").style.order="3";
+    document.getElementById("box4").style.order="4";
+    document.getElementById("box5").style.order="5";
+    document.getElementById("box6").style.order="6";
+    document.getElementById("box7").style.order="7";
+    document.getElementById("box8").style.order="8";
+    document.getElementById("box9").style.order="9";
+}
